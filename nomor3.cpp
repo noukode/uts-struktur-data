0dc4d6935395f70d18ad496c5456940ef71bae89
Hilmi Maulana Mukhlis <maulanahilmi909@gmail.com>
2023-06-02 16:57:55 +0700

    update: penjelasan nomor3

--- nomor3.cpp
+++ nomor3.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
+
+    //declare stack 1
     stack<int> stack_satu;
     stack_satu.push(1);
     stack_satu.push(2);
@@ -12,6 +14,7 @@ int main(){
     stack_satu.push(5);
     stack_satu.push(6);
     
+    //declare stack 2
     stack<int> stack_dua;
     stack_dua.push(6);
     stack_dua.push(5);
@@ -20,19 +23,25 @@ int main(){
     stack_dua.push(2);
     stack_dua.push(1);
     
+    //pengecekan ukuran / panjang stack
     if (stack_satu.size() != stack_dua.size()){
         cout << "Stack satu dan Stack dua tidak memiliki nilai yang sama";
     }else{
+        //looping stack selama stack 1 masih memiliki value
         bool status = true;
         while(!stack_satu.empty()){
+            // pengecekan tiap value antara stack 1 dan 2
             if(stack_satu.top() != stack_dua.top()){
                 status = false;
                 break;
             }else{
+                //menghapus 1 value dari stack
                 stack_satu.pop();
                 stack_dua.pop();
             }
         }
+
+        ///pengecekan hasil status dari pengecekan tiap value di dalam stack
         if(status){
             cout << "Stack satu dan Stack dua memiliki nilai yang sama";
         }else{
